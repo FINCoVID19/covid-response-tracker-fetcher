50de1b139cef8028eb95c3a590d820028b466a94
Måns Magnusson <mons.magnusson@gmail.com>
2020-04-19 11:39:28 +0300

    Update package

--- R/covidtracker.R
+++ R/covidtracker.R
@@ -12,6 +12,7 @@
 #' @param from the first date to return data for
 #' @param to the last date to return data for
 #' @param date the date to return data for
+#' @param dates multiple dates to return data for
 #' @param country a ISO 3166-1 alpha-3 country code
 #' @param countries a vector of ISO 3166-1 alpha-3 country codes
 #' @param verbose show progress of data download
@@ -51,14 +52,23 @@ covidtracker_actions <- function(country, date){
 
 #' @rdname covidtracker_date_range
 #' @export
-covidtracker_actions_range <- function(countries = c("FIN", "SWE"), from = "2020-03-01", to = "2020-03-05", verbose = TRUE){
+covidtracker_actions_range <- function(countries, from, to, verbose = TRUE){
   checkmate::assert_character(countries, pattern = "[A-Za-z]{3}")
   from <- as.Date(from); to <- as.Date(to)
   checkmate::assert_date(from)
   checkmate::assert_date(to)
   checkmate::assert_true(from <= to)
   date_seq <- seq(from, to, by = 1)
-  gr <- expand.grid(country = countries, date = date_seq, stringsAsFactors = FALSE)
+  covidtracker_actions_dates(countries, date_seq, verbose)
+}
+
+#' @rdname covidtracker_date_range
+#' @export
+covidtracker_actions_dates <- function(countries, dates, verbose = TRUE){
+  checkmate::assert_character(countries, pattern = "[A-Za-z]{3}")
+  dates <- as.Date(dates)
+  checkmate::assert_date(dates, unique = TRUE)
+  gr <- expand.grid(country = countries, date = dates, stringsAsFactors = FALSE)
 
   if(verbose) pb <- txtProgressBar(min = 1, max = nrow(gr), style = 3)
   cta_list <- list()
@@ -69,18 +79,47 @@ covidtracker_actions_range <- function(countries = c("FIN", "SWE"), from = "2020
   cta_list
 }
 
+#' Extract data from covidtracker objects
+#'
+#' @param x a [covidtracker_actions] object to extract data from
+#' @param ... further arguments to methods.
+#'
+#' @details
+#' [data_frame_actions] extract policy actions together with date and country code
+#' [data_frame_stringency] extract cases, deaths and stringency index together with date and country code
+#'
 #' @export
-as.data.frame.covidtracker_actions <- function(x, ...){
+data_frame_actions <- function(x, ...){
+  UseMethod("data_frame_actions")
+}
+
+#' @export
+data_frame_actions.list <- function(x, ...){
+  x <- lapply(x, data_frame_actions)
+  do.call(rbind, x)
+}
+
+#' @export
+data_frame_actions.covidtracker_actions <- function(x, ...){
   # Replace NULL with ""
   xpa <- lapply(x[["policyActions"]], function(x) lapply(x, function(x) if(is.null(x)) return("") else return(x)))
   xpa <- do.call(rbind, lapply(xpa, as.data.frame, stringsAsFactors = FALSE))
   xpa$date <- x$stringencyData$date_value
   xpa$country_code <- x$stringencyData$country_code
+  nc <- ncol(xpa)
+  xpa <- xpa[, c((nc - 1):nc, 1:(nc-2))]
+  xpa
+}
+
+#' @rdname data_frame_actions
+#' @export
+data_frame_stringency <- function(x){
+  checkmate::assert_class(x, "covidtracker_actions")
+  xpa <- data.frame(date = x$stringencyData$date_value)
+  xpa$country_code <- x$stringencyData$country_code
   xpa$confirmed <- x$stringencyData$confirmed
   xpa$deaths <- x$stringencyData$deaths
   xpa$stringency_actual <- x$stringencyData$stringency_actual
   xpa$stringency <- x$stringencyData$stringency
-  nc <- ncol(xpa)
-  xpa <- xpa[, c((nc - 5):nc, 1:(nc-6))]
   xpa
 }
---
+++ R/covidtracker_csv.R
@@ -0,0 +1,54 @@
+#' Create and store API data as CSV files
+#'
+#' @param file csv-file to store or update
+#' @inheritParams covidtracker_date_range
+#'
+#' @importFrom utils read.csv write.csv
+#'
+#' @details
+#' [covidtracker_create_actions_csv()] create a csv file to store response information in
+#' [covidtracker_create_update_csv()] update an existing csv file with new data
+#'
+#' @export
+covidtracker_update_actions_csv <- function(file, from = NULL, to = Sys.Date(), verbose = TRUE){
+  checkmate::assert_file_exists(file)
+  from <- as.Date(from); to <- as.Date(to)
+  checkmate::assert_date(from, null.ok = TRUE)
+  checkmate::assert_date(to)
+
+  x <- read.csv(file, stringsAsFactors = FALSE)
+  checkmate::assert_names(colnames(x), must.include = c("date", "country_code", "policy_type_code"))
+  colnames <- colnames(x)
+  countries <- unique(x$country_code)
+  dates <- as.Date(unique(x$date))
+  x[is.na(x)] <- ""
+  if(is.null(from)) from <- dates[which.min(dates)]
+
+  update_dates <- seq(from, to, by = 1)
+  update_dates <- update_dates[!update_dates %in% dates]
+
+  ctad <- covidtracker_actions_dates(countries, update_dates, verbose = TRUE)
+  ctad <- data_frame_actions(ctad)
+  ctad <- ctad[,colnames]
+
+  x <- rbind(x, ctad)
+  x[order(x$date, x$country_code, x$policy_type_code),]
+}
+
+#' @rdname covidtracker_update_actions_csv
+#' @export
+covidtracker_create_actions_csv <- function(file, countries, from, to, verbose = TRUE){
+  checkmate::assert_path_for_output(file)
+  checkmate::assert_character(countries, pattern = "[A-Za-z]{3}")
+  from <- as.Date(from); to <- as.Date(to)
+  checkmate::assert_date(from)
+  checkmate::assert_date(to)
+  checkmate::assert_flag(verbose)
+
+  cta <- covidtracker_actions_range(countries, from, to, verbose)
+  cta <- data_frame_actions(cta)
+  cta$notes <- NULL
+  cta <- cta[order(cta$date, cta$country_code, cta$policy_type_code),]
+  write.csv(cta, file = "interventions_oxford.csv", row.names = FALSE)
+
+}
